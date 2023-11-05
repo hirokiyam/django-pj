855bebd793fb9682a1349406291c2cc434751d47
hirokiyam <distant_record@hotmail.co.jp>
2023-11-05 19:02:57 +0900

    アプリを作成

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'djangopj.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # SQLite用（デフォルト）
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
